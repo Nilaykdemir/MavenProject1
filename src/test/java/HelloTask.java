cb17bec6731f43a0ecc077499e50226a8561893d
Nilay Keser Demir <nilaykdemir@gmail.com>
2021-12-29 23:43:39 +0000

    more Hello Feature added

--- src/test/java/HelloTask.java
+++ src/test/java/HelloTask.java
@@ -3,5 +3,7 @@ public class HelloTask {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Developing Hello Feature");
+        System.out.println("Local Repo is already published in GitHub ");
+        System.out.println("More Work Done " );
     }
 }
